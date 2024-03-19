6dbb23fb62954b165e14d6bf023e09c8b889f1a9
Muhammad Ahmad <ahmadmuhdpk@gmail.com>
2024-03-20 02:07:42 +0500

    vercel added

--- routes/project.js
+++ routes/project.js
@@ -9,7 +9,7 @@ const {
 var router = express.Router();
 const { isCompanyAuthenticated, isAuthenticated } = require("../middlewares/auth");
 
-router.route("/project/post").post( createProject);
+router.route("/project/post").post( isCompanyAuthenticated,createProject);
 router.route("/project/getProjects").get(getAllProjects);
 router.route("/project/searchProject").post(searchProjects);
 router.route("/project/:id").delete(isCompanyAuthenticated, deleteProject);
